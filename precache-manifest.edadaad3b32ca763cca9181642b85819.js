d12de0513cf4d1f26030f7f4afc4b2e736c133c7
Ernest <perkowski.ernest98@gmail.com>
2020-10-10 23:35:25 +0200

    Updates

---
+++ precache-manifest.edadaad3b32ca763cca9181642b85819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa8b9961225c9bffe62ada2d0b2007",
+    "url": "/chuck-norris/index.html"
+  },
+  {
+    "revision": "ddc98af453270d71dcb2",
+    "url": "/chuck-norris/static/css/main.7a39dc7a.chunk.css"
+  },
+  {
+    "revision": "27ef0ce43c128c2e3200",
+    "url": "/chuck-norris/static/js/2.4b05494e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris/static/js/2.4b05494e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc98af453270d71dcb2",
+    "url": "/chuck-norris/static/js/main.08bba491.chunk.js"
+  },
+  {
+    "revision": "9ef1b9caa31a2f0a9dae",
+    "url": "/chuck-norris/static/js/runtime-main.5a2d1b87.js"
+  }
+]);
